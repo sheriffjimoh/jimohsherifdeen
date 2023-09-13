588cbc1c58c6b0473fd6f6282f80e6f163867551
sheriffjimoh@gmail.com <sheriffjimoh@gmail.com>
2023-09-13 18:35:03 +0100

    fix: view

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -160,17 +160,17 @@ export async function getStaticProps({ params }: any) {
     },
   })
 
-  const views = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/views/${params.slug}`,
-  )
-    .then((res) => res.json())
-    .then((json) => json.views)
+  // const views = await fetch(
+  //   `${process.env.NEXT_PUBLIC_BASE_URL}/api/views/${params.slug}`,
+  // )
+  //   .then((res) => res.json())
+  //   .then((json) => json.views)
 
   return {
     props: {
       metadata: article,
       source: mdxSource,
-      views: views ? views :  0,
+      views:  0,
     },
     revalidate: 30,
   }
